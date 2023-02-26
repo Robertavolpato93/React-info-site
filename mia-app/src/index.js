279bcf92741ca2b01e8156cb786a5795b72542c9
Roberta <robi.volpato93@gmail.com>
2023-02-26 15:12:08 +0100

    scaricato react e iniziato sito

---
+++ mia-app/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+ const Page =(
+   <div>
+   <nav><h1>Tech-Mommy</h1>
+  <ul><li>Info</li><li>About</li><li>Contact</li></ul>
+  </nav>
+</div>
+ )
+ReactDOM.render(
+Page, 
+document.getElementById ("root")
+)
