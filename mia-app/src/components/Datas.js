8567d9e817f760c2f7664d59f2f14affd350cbdf
Roberta <robi.volpato93@gmail.com>
2023-03-06 23:20:00 +0100

    useState prob

---
+++ mia-app/src/components/Datas.js
@@ -0,0 +1,58 @@
+ export default {
+    "success": true,
+    "datas":{
+     "place":[
+    {
+        "id":"1",
+       "url" :"https://www.dove-e-quando.it/site/images/illustration/londres_830.jpg",
+        "placeName":"Londra",
+        "continent":"Europa"
+
+    },
+    {
+        "id":"2",
+       "url" :"https://assets.cntraveller.in/photos/63ad209f79d81704e445de5d/master/w_1600%2Cc_limit/The%2520Burj%2520Khalifa-GettyImages-1084264582.jpeg",
+        "placeName":"Dubai",
+        "continent":"Asia"
+
+    },
+    {
+        "id":"3",
+       "url" :"https://www.telegraph.co.uk/content/dam/Travel/Destinations/North%20America/USA/New%20York/Attractions/statue-of-liberty-new-york-p.jpg",
+        "placeName":"New York",
+        "continent":"USA"
+
+    },
+    {
+        "id":"4",
+       "url" :"https://www.costacrociere.it/content/dam/costa/costa-magazine/articles-magazine/heritage/egyptian-pyramids/piramidi-egitto_m.jpg.image.694.390.low.jpg",
+        "placeName":"El Cairo",
+        "continent":"Africa"
+
+    },
+    {
+        "id":"5",
+       "url" :"https://allarremviaggio.com/wp-content/uploads/2020/11/australia-sydney-opera-house-harbour-bridge-1918x959.jpg",
+        "placeName":"Sidney",
+        "continent":"Oceania"
+
+    },
+    {
+        "id":"6",
+       "url" :"https://images.vanityfair.it/Storage/Assets/Crops/357877/53/185579/rovaniemi-finlandia-babbo-natale_660x380.jpg",
+        "placeName":"Rovaniemi",
+        "continent":"Polo Nord"
+
+    },
+    {
+        "id":"7",
+       "url" :"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/7c/bd/6b/hotel-maitai-bora-bora.jpg?w=600&h=400&s=1",
+        "placeName":"Caraibi",
+        "continent":"Sud America"
+
+    },
+    
+    
+]
+}
+ }
--- mia-app/src/components/Form.js
+++ mia-app/src/components/Form.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Datas from "./Datas";
+
+
+
 export default function Form() {
+const [PlaceImage, setPlaceImage]= useState("");
+
+function getPlaceImage(){
+  const placeArray = Datas.datas.place
+  const randomNumber = Math.floor(Math.random() * placeArray.length)
+  setPlaceImage(placeArray[randomNumber].url)
+}
   return (
-    <div className="form">
-      
+   <main>
+      <div className="form">
         <h3 className="h3-form"> Inserisci le tue preferenze,<br/> al resto ci pensiamo noi!</h3>
         <input type="text" placeholder="paese" />
         <br/>
         <input type="text" placeholder="Mare, montagna, città etc..." />
         <br/>
-        <button type="submit">TROVA</button>
+        <button  onclick={getPlaceImage} className="form-button" >TROVA</button>
+       <img  className="form-img" src={PlaceImage} />
+        </div>
         
-      
-    </div>
-  );
+   </main>
+    
+        );
 }
